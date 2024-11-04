6143c82a99ad8a42d0037d45bf606862c364d390
DellX <a.razzaq2042@gmail.com>
2024-11-04 22:32:33 +0300

    My First Program

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -3,7 +3,7 @@ import org.apache.spark.SparkContext
 object hello {
   def main(args:Array[String]):Unit=
     {
-      val sc=new SparkContext(("local[*]"),"razzaq")
+      val sc=new SparkContext("local[*]","razzaq")
       val input=sc.textFile("C:/Users/DellX/Desktop/ForPractice/data.txt")
          val rdd1=input.flatMap(x=>x.split(" "))
          val rdd2=rdd1.map(x=>(x,1))
