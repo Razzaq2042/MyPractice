0cf4ebeabcc4895d8cbd1f06fb8c373297c36b13
DellX <a.razzaq2042@gmail.com>
2024-11-04 22:31:38 +0300

    My First Program

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -1,7 +1,18 @@
+import org.apache.spark.SparkContext
+
 object hello {
   def main(args:Array[String]):Unit=
     {
-      println("hello world")
+      val sc=new SparkContext(("local[*]"),"razzaq")
+      val input=sc.textFile("C:/Users/DellX/Desktop/ForPractice/data.txt")
+         val rdd1=input.flatMap(x=>x.split(" "))
+         val rdd2=rdd1.map(x=>(x,1))
+         val rdd3=rdd2.reduceByKey((x,y)=>x+y)
+         val rdd4=rdd3.sortBy(x=>x._2,false)
+         rdd4.take(2).foreach(println)
+
+//      scala.io.StdIn.readLine()
+
     }
 
 }
