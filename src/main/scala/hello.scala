b29d1d9fad33593a6c6121751423ca577028e693
DellX <a.razzaq2042@gmail.com>
2024-11-04 22:33:06 +0300

    My First Program

--- src/main/scala/hello.scala
+++ src/main/scala/hello.scala
@@ -8,7 +8,7 @@ object hello {
          val rdd1=input.flatMap(x=>x.split(" "))
          val rdd2=rdd1.map(x=>(x,1))
          val rdd3=rdd2.reduceByKey((x,y)=>x+y)
-         val rdd4=rdd3.sortBy(x=>x._2,false)
+         val rdd4=rdd3.sortBy(x=>x._2,ascending = false)
          rdd4.take(2).foreach(println)
 
 //      scala.io.StdIn.readLine()
